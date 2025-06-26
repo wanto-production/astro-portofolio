71d564f87c61c6f060da96c4be593e463fa8cbf9
wanto-production <ikwansatria3974@gmail.com>
2025-06-26 17:29:50 +0700

    initial commit

--- src/components/solidjs/motionImage.tsx
+++ src/components/solidjs/motionImage.tsx
@@ -1,14 +1,17 @@
 import { Motion } from 'solid-motionone';
+import "lazysizes";
+import "lazysizes/plugins/blur-up/ls.blur-up";
 
 export default function MotionImage() {
   return (
     <Motion.img
       initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 100, y: 0 }}
-      transition={{ easing: 'ease-out' }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ easing: 'ease-out', duration: 0.6 }}
       hover={{ scale: 1.02 }}
-      src="/static/nishimiya.png"
-      class="rounded-2xl shadow-2xl w-full h-[350px] object-cover object-center"
+      data-src="/static/nishimiya.png"
+      alt="Nishimiya"
+      class="lazyload blur-up rounded-2xl shadow-2xl w-full h-[350px] object-cover object-center"
     />
   );
 }
